fd951802eca76a4f2984ade77b5fab2baf2942dd
WuXiuChaoPlus <383137597@qq.com>
2018-11-14 16:45:11 +0800

    update

--- model.py
+++ model.py
@@ -20,6 +20,11 @@ class RNN(object):
 	self.embedding_size = embedding_size # 嵌入映射大小
 
 	tf.reset_default_graph() # 重置图
+	self.build_inputs()
+	self.build_lstm()
+	self.build_loss()
+	self.build_opt()
+	self.saver = tf.train.Saver()
     
     """
     输入层 
@@ -64,7 +69,7 @@ class RNN(object):
 				W = tf.Variable(tf.truncated_normal([self.num_units,self.features], stddev=0.1))
 				b = tf.Variable(tf.zeros(self.features))
 
-			self.logits = tf.matmul(x,W) + b # 结果shape为 
+			self.logits = tf.matmul(x,W) + b # 结果shape为 [batch_size*max_time, num_units]
 			self.prediction = tf.nn.softmax(self.logits,name='prediction')
 	"""
 	loss层
@@ -74,3 +79,48 @@ class RNN(object):
 			y_one_hot = tf.one_hot(self.targets, self.features) #把targets按features展开
 			y_label = tf.reshape(y_one_hot, self.logits.get_shape()) #把label的shape变成和logits一致
 			self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=y_label))
+
+	"""
+	opt层
+	"""
+	def build_opt(self):
+		"""
+		１．在solver中先设置一个grad_clip
+		２．在前向传播与反向传播之后，我们会得到每个权重的梯度diff，这时不像通常那样直接使用这些梯度进行权重更新，
+		而是先求所有权重梯度的平方和sumsq_diff，如果sumsq_diff > grad_clip，则求缩放因子scale_factor = clip_gradient / sumsq_diff。
+		这个scale_factor在(0,1)之间。如果权重梯度的平方和sumsq_diff越大，那缩放因子将越小。 
+		３．最后将所有的权重梯度乘以这个缩放因子，这时得到的梯度才是最后的梯度信息。
+		这样就保证了在一次迭代更新中，所有权重的梯度的平方和在一个设定范围以内，这个范围就是grad_clip。
+		出自博客:https://blog.csdn.net/u013713117/article/details/56281715
+		"""
+		tvars = tf.trainable_variables()
+		grads, _ = tf.clip_by_global_norm(tf.gradients(self.loss,tvars),self.grad_clip)
+		train_op = tf.train.AdamOptimizer(self.learning_rate)
+		self.optimizer =  train_op.apply_gradients(zip(grads,tvars))
+
+	"""
+	训练
+	batch_generator 生成器
+	max_steps 迭代次数
+	save_path 模型保存路径
+	"""
+	def train(self, batch_generator, max_steps, save_path):
+		self.session = tf.Session()
+		with self.session as sess:
+			sess.run(tf.global_variable_initializer()) #初始化全局变量
+			new_state = sess.run(self.initial_state)
+			step = 0
+			for x,y in batch_generator:
+				step += 1
+				feed = {self.inputs:x,self.targets:y,self.keep_prob:self.train_keep_prob,self.initial_state:new_state}
+				batch_loss, new_state, _ = sess.run([self.loss,self.final_state,self.optimizer],feed_dict=feed)
+				if step % 100 == 0:
+					print('step: {}/{}------------>loss: {:.4f}'.format(step,max_steps,batch_loss))
+				if step >= max_steps:
+					break
+			self.saver.save(sess,os.path.join(save_path,'model'),global_step=step)
+
+	# def sample(self, n_samples, prime, vocab_size):
+
+
+
